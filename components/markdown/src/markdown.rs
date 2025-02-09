a2760b9accca6b893a33deadec25574fc8ed12c7
cstef <colin@petit-suisse.fr>
2025-02-09 11:12:07 +0100

    fix: math engine `none` panic

--- components/markdown/src/markdown.rs
+++ components/markdown/src/markdown.rs
@@ -793,6 +793,7 @@ pub fn markdown_to_html(
                                 }
                             }
                         }
+                        config::MathRendering::None => {}
                         e => todo!("Unsupported math rendering: {:?}", e),
                     }
                 }

9574c8d9bede27cd8f46a54a77dbe5a5e3c45af2
cstef <root@cstef.dev>
2025-02-19 10:23:12 +0100

    fix: parent absolute when the section is at tricky locations

--- components/markdown/src/markdown.rs
+++ components/markdown/src/markdown.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fmt::Write;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use crate::callouts::ObsidianCalloutsHandler;
 use crate::markdown::cmark::CowStr;
@@ -633,15 +633,7 @@ pub fn markdown_to_html(
                 Event::End(TagEnd::CodeBlock { .. }) => {
                     if let Some(ref mut code_block) = code_block {
                         let inner = code_block
-                            .include(
-                                context
-                                    .parent_absolute
-                                    .map(|e| {
-                                        path.map(|p| e.join(PathBuf::from(p).parent().unwrap()))
-                                    })
-                                    .flatten()
-                                    .as_ref(),
-                            )
+                            .include(context.parent_absolute.as_ref())
                             .unwrap_or(accumulated_block.clone());
                         match code_block_language.as_deref() {
                             Some("typ") => {
